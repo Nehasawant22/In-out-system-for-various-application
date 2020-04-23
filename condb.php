0b72f8d23c3ab0953d23474fcb4b0e698f1d40a1
Nehasawant22 <61915310+Nehasawant22@users.noreply.github.com>
2020-04-23 20:24:17 +0530

    Update condb.php

--- condb.php
+++ condb.php
@@ -7,5 +7,5 @@ $dbname = "lib";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);  //neha sawant
 }
